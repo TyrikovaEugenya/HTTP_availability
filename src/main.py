80c341cf306899941d1ec55c0d305972a1ef96a5
Eugene <et12sova@gmail.com>
2025-02-09 16:21:55 +0700

    all done

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
-import http
 import requests
 import argparse
 import re
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-H', '--host', type=str)
-    parser.add_argument('-C', '--count', type=int, default=1)
-    parser.add_argument('-F', '--file', type=str)
-    parser.add_argument('-O', '--output', type=str, default='console')
+    parser.add_argument('-H', '--host', type=str, help='Comma-separated list of hosts to test.')
+    parser.add_argument('-C', '--count', type=int, default=1, help='Number of requests to send to each host (default: 1).')
+    parser.add_argument('-F', '--file', type=str,  help='Path to a file containing hostnames (cannot be used with -H).')
+    parser.add_argument('-O', '--output', type=str, default='console', help='File to save the output (default: output is displayed in the console).')
 
     args = parser.parse_args()
 
@@ -42,16 +41,30 @@ def getting_response(host, number_of_calls, args):
         max_time = max(times)
     else:
         min_time = max_time = avg_time = 0
+
     output(host, success_count, failed_count,
            errors_count, min_time, max_time, avg_time, args)
 
 def output(host_name, success_count, failed_count,
            errors_count, min_time, max_time, avg_time, args):
+    result = (
+        f'Host: {host_name}\n'
+        f'Successful requests: {success_count}\n'
+        f'Failed requests: {failed_count}\n'
+        f'Errors: {errors_count}\n'
+        f'Min/Max response time: {min_time:.3f}/{max_time:.3f} seconds\n'
+        f'Average response time: {avg_time:.3f} seconds\n')
+
     if args.output == 'console':
-        print(f'''\nHost_name: {host_name}\nNumber of successed request: {success_count}\nNumber of failed request: {failed_count}\nNumber of errors: {errors_count}\nMinimum/Maximum elapsed time: {min_time}/{max_time}\nAverage elapsed time: {avg_time}.\n''')
+        print(result)
     else:
-        with open(args.output, 'w') as file:
-            file.write(f'''\nHost_name: {host_name}\nNumber of successed request: {success_count}\nNumber of failed request: {failed_count}\nNumber of errors: {errors_count}\nMinimum/Maximum elapsed time: {min_time}/{max_time}\nAverage elapsed time: {avg_time}.\n''')
+        try:
+            with open(args.output, 'w') as file:
+                file.write(result)
+        except IOError as e:
+            print(f'Error writing to file {args.output}: {e}')
+            exit(1)
+            
 
 def try_match_host(list_of_requests):
     pattern = r'^(https?://)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(/.*)?$'
@@ -64,8 +77,9 @@ def gettimg_hosts_from_file(path):
     try:
         with open(path, 'r') as file:
             s = [line.strip('\n') for line in file.readlines()]
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         print('Error: source file not found')
+        exit(1)
     return s
           
 
@@ -77,12 +91,16 @@ if __name__ == '__main__':
         
     if args.file:
         list_of_requests = gettimg_hosts_from_file(args.file)
-    else:
+    elif args.host:
         try:
             list_of_requests = args.host.split(',')
         except AttributeError as e:
             print('After key -H must be at least one adress')
             exit(1)
+    else:
+        print('Error: You must specify at least one host using -H or -F.')
+        exit(1)
+
     number_of_calls = args.count
     try_match_host(list_of_requests)
 
